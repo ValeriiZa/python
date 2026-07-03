eaf17f9eeddd34f59aa4263189f988842c03268a
ValeriiZa <59182913+ValeriiZa@users.noreply.github.com>
2026-07-03 20:03:00 +0300

    Update sorting.py
    Finished Quick Sort

--- sorting.py
+++ sorting.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python3
 import time, sys
-#from datetime import now
 from datetime import datetime
 
 #print (sys.version)
@@ -50,6 +49,7 @@ def bubble_desc (arr):
 
 ##################################################
 ###################   selection   ################
+##################################################
 def selection (arr):
     t = datetime.now()
     arr_size = len(arr)
@@ -63,10 +63,11 @@ def selection (arr):
             arr[i] = arr[min]
             arr[min] = temp
         #print (arr)
-    print ("Selection t:", datetime.now()-t)
+    print ("Selection sort time:", datetime.now()-t)
     
 ##################################################
 ###################   Insertion   ################
+##################################################
 def insertion (arr):
     t = datetime.now()
     arr_size = len(arr)
@@ -78,7 +79,7 @@ def insertion (arr):
             arr[j+1] = temp
             j=j-1
         print (arr)
-    print ("Insertion t:", datetime.now()-t)
+    print ("Insertion sort time:", datetime.now()-t)
 
 def swap (arr, n, m):
     temp = arr[n]
@@ -96,7 +97,8 @@ def split (arr):
             arr = swap (arr, 0, 1)
             
 ##################################################
-######### Merge sort Start #########
+############### Merge sort Start #################
+##################################################
 def joinArr (a, left, mid, right):
     # 1. count lenth of each part;
     len1 = mid - left + 1
@@ -146,12 +148,12 @@ def mergeArr (a, left, right):
 def merge (arr):
     return mergeArr (arr, 0, len(arr)-1)
 
-######### Merge sort End #########
+################## Merge sort End ##################
 
 
 
-#######  Quick Sort #######
-arr = [3, 2,  1, 7,  4, 0,  6, 8, 3]
+##################   Quick Sort   ##################
+arr = [3, 2,  1,  7, 4] #, 0,  6, 8, 3]
 def sort ( arr, start=0, finish=None ):
     # 1. Select an element from the array as the pivot (at center).
     if finish == None:
@@ -162,9 +164,6 @@ def sort ( arr, start=0, finish=None ):
     l = []
     r = []
     middle = arr[pivot]
-    print ("_________ arr:", arr)
-    print ("___> start + finish", start, finish)
-    print ("pivot, arr[pivot]", pivot, arr[pivot])
     for i in range(start, finish+1):
         if i == pivot:
             continue
@@ -172,23 +171,19 @@ def sort ( arr, start=0, finish=None ):
             l.append(arr[i])
         else:
             r.append(arr[i])
-    print ("L: ", l)
-    print ("R: ", r)
-    print ("arr[pivot]: ", middle)
     for i in range (len(l)):
         arr[ start+i ] = l[i]
     arr[ start+len(l) ] = middle
     for i in range (len(r)):
         arr[ start + len(l) + 1 + i ] = r[ i ]
-
 # 3. Recursively apply the same process to the two partitioned sub-arrays.
 # 4. The recursion stops when there is only one element left in the sub-array,
 #    as a single element is already sorted.
-    if ( len(l)>1 ) :
-        sort (arr, start, len(l)-1)
-    print ("pivot+1 < finish)", pivot+1, finish, "\n----------\n")
-    if (len(r) > 1):
-        sort (arr, len(l)+2, finish)
+    if ( len(l) > 1 ) :
+        sort (arr, start, start+len(l)-1)
+    if ( len(r) > 1 ):
+        sort (arr, start+len(l)+1, start+len(l)+len(r))
+
     return arr
 
 ###########################
@@ -201,12 +196,12 @@ def sort ( arr, start=0, finish=None ):
 
 arr = [3, 2,  1, 7,  4, 0,  6, 8, 3]
 ##print (merge (arr))
-print ("Arr before using SORT method: ", arr)
-print ("Arr after using SORT method: ", sort (arr) )
+#print ("Arr before using SORT method: ", arr)
+#print ("Arr after using SORT method: ", sort (arr) )
 
 arr = [123, 1, 23, -32]
 arr1 = [123,1,23,-32,123,3,-534,64,63,9,12,5,19,11,63,23,1,5,82,9,43,123,44,33,2,1,0,-2]
-               
+print ("Arr after using SORT method: ", sort (arr1) )
 #bubble_asc  (arr1)
 #bubble_desc (arr1)
 
